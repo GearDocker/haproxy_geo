229551331426e499df90dd6811b211ee5db63559
Gary <gear@thytruth.com>
2017-11-02 21:19:54 -0700

    added prepopulate geoip

--- populate_geo.sh
+++ populate_geo.sh
@@ -9,5 +9,3 @@ wget http://dev.maxmind.com/static/csv/codes/country_continent.csv
 for c in `grep -E -v "\-|iso" country_continent.csv | sort -t',' -k 2` ; do echo $c | awk -F',' '{ print $1 >> $2".continent" }' ; done
 cut -d, -f1,2,5 GeoIPCountryWhois.csv | iprange | sed 's/"//g' | awk -F' ' '{ print $1 >> $2".subnets" }'
 for f in `ls *.continent`; do for c in $(cat $f); do [[ -f ${c}.subnets ]] && cat ${c}.subnets >> ${f%%.*}.txt; done; done
-
-sleep 90000000
